8addbbc40ede0f0d3ee8dd72bcbef9e4bc73fc0d
beno1234 <benolopesdias@gmail.com>
2023-07-13 11:09:42 -0300

    teste para deploy windows

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#01399b] py-6 px-4 md:px-8 lg:px-16">
+    <footer className="bg-[#40845b] py-6 px-4 md:px-8 lg:px-16">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="text-white text-center md:text-left mb-4 md:mb-0">
