7b4c15155c7ada62c08f4bcefcf4078f8198efe6
beno1234 <benolopesdias@gmail.com>
2023-07-14 11:28:42 -0300

    tomei cafe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,19 +7,19 @@ const Header = () => {
           backgroundPosition: "center center",
           backgroundSize: "cover",
         }}
-        className="h-[100vh] absolute w-full bg-no-repeat bg-cover corner-shapes-wrap"
+        className="h-screen md:h-full absolute w-full bg-no-repeat bg-cover corner-shapes-wrap"
       >
         <div className="corner-shapes"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative    ">
+        <div className="relative">
           <div className="mx-auto text-center">
             <div className="mb-0">
               <img
                 src="/logoAmazonas2.png"
-                className="mx-auto w-[40%] mb-0"
+                className="mx-auto w-[40%] md:w-[30%] mb-0"
                 alt="Logo Amazonas"
               />
-              <h2 className="text-white text-4xl font-bold ">
+              <h2 className="text-white text-4xl font-bold mt-4">
                 O lugar perfeito para construir os seus sonhos. <br />
                 Lançamento em breve!
               </h2>
@@ -28,7 +28,7 @@ const Header = () => {
                   LOTES
                 </h3>
               </div>
-              <div className="mx-auto  w-56 mt-2 rounded-lg shadow-xl">
+              <div className="mx-auto w-56 md:w-80 mt-2 rounded-lg shadow-xl">
                 <h3 className="bg-[#4d8a3f] font-montserrat p-1 text-white text-3xl">
                   A PARTIR DE <br />
                   <span className="text-5xl">160m²</span>
