a7ce289bfd5325664dd56b5faea105e70fd0d7ce
beno1234 <benolopesdias@gmail.com>
2023-07-14 10:31:02 -0300

    acordado estou

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,27 +7,33 @@ const Header = () => {
           backgroundPosition: "center center",
           backgroundSize: "cover",
         }}
-        className="h-screen md:h-full absolute w-full bg-no-repeat bg-cover corner-shapes-wrap"
+        className="h-[100vh] absolute w-full bg-no-repeat bg-cover corner-shapes-wrap"
       >
         <div className="corner-shapes"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative h-full flex justify-center items-center">
+        <div className="relative h-full flex  ">
           <div className="container mx-auto text-center">
             <div className="mt-0 pt-0">
               <img
                 src="/logoAmazonas2.png"
-                className="mx-auto w-[50%] md:w-auto"
+                className="mx-auto w-[50%]"
                 alt="Logo Amazonas"
               />
               <h2 className="text-white text-4xl font-bold mt-4">
                 O lugar perfeito para construir os seus sonhos. <br />
                 Lançamento em breve!
               </h2>
-              <img
-                src="/lote.jpeg"
-                className="mx-auto mt-8"
-                alt="Imagem de lote"
-              />
+              <div className="mx-auto w-min mt-10 rounded-lg shadow-xl">
+                <h3 className="bg-[#4d8a3f] font-montserrat p-1 text-white text-5xl">
+                  LOTES
+                </h3>
+              </div>
+              <div className="mx-auto  w-56 mt-2 rounded-lg shadow-xl">
+                <h3 className="bg-[#4d8a3f] font-montserrat p-1 text-white text-3xl">
+                  A PARTIR DE <br />
+                  <span className="text-5xl">160m²</span>
+                </h3>
+              </div>
             </div>
           </div>
         </div>
